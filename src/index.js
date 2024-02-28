2a4015d4b478ea0177542b0ad8f5effe413640dd
maryamnora <m.ahmadi021@gmail.com>
2024-02-28 18:34:58 -0500

    added time

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 function tempp(response) {
-  let tempRes = response.data.temperature.current;
-
   let tempUpdate = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
   let conditionElement = document.querySelector("#condition");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let tempRes = response.data.temperature.current;
+  tempUpdate.innerHTML = Math.round(tempRes);
+  cityElement.innerHTML = response.data.city;
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
   let Roundwind = response.data.wind.speed;
   windElement.innerHTML = Math.round(Roundwind);
-  humidityElement.innerHTML = response.data.temperature.humidity;
-  conditionElement.innerHTML = response.data.condition.description;
-  cityElement.innerHTML = response.data.city;
-  tempUpdate.innerHTML = Math.round(tempRes);
+  timeElement.innerHTML = time(date);
+}
+function time(date) {
+  let hour = date.getHours();
+  let mins = date.getMinutes();
+  let Days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = Days[date.getDay()];
+  if (mins < 10) {
+    mins = `0${mins}`;
+  }
+  return `${day}, ${hour}:${mins}`;
 }
 
 function URL(city) {
