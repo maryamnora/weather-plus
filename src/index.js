647b03683b3f30fba1ca171813ae1c6dd59cf5c0
maryamnora <m.ahmadi021@gmail.com>
2024-02-28 16:32:16 -0500

    added serach functionality with API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function tempp(response) {
+  let tempElement = response.data.temperature.current;
+  let tempUpdate = document.querySelector("#temp");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  tempUpdate.innerHTML = Math.round(tempElement);
+}
+
+function URL(city) {
+  let apikey = "6015b50d3adc47t42c1668fbad7435o2";
+  let apiurl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+  axios.get(apiurl).then(tempp);
+}
+
 function cityChange(event) {
   event.preventDefault();
   let inputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = inputElement.value;
+
+  URL(inputElement.value);
 }
 let formElement = document.querySelector("#form");
 formElement.addEventListener("submit", cityChange);
